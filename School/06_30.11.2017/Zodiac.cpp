b4a6cdad329abbb19b0053292a20e2df8486737e
quakeN <quakendev@gmail.com>
2017-12-02 23:10:52 +0200

    Fixed some code.

--- School/06_30.11.2017/Zodiac.cpp
+++ School/06_30.11.2017/Zodiac.cpp
@@ -11,20 +11,7 @@ int main()
 	cout << "Enter day: ";
 	cin >> day;
 
-	switch(month)
-	case 1: if (day >= 21 && day <= 19)cout << "Vodolei";
-
-	case 2: cout << "xd";
-	case 3: cout << "xd";
-	case 4: cout << "xd";
-	case 5: cout << "xd";
-	case 6: cout << "xd";
-	case 7: cout << "xd";
-	case 8: cout << "xd";
-	case 9: cout << "xd";
-	case 10: cout << "xd";
-	case 11: cout << "xd";
-	case 12: cout << "xd";
+	//TODO
 
 	system("pause");
 	return 0;
