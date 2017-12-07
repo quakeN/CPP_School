a42dc0471be2e601e1e1011f5af9bcd1a5925f6c
Georgi Slaweykow <quakendev@gmail.com>
2017-12-07 16:29:21 +0200

    Uploading.

---
+++ School/08_07.12.2017/FootbalPlayers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main()
+{
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
+
+	if(number == 11)
+	{
+		cout << "Attacker" << endl;
+	}
+	else
+	{
+		switch(number % 10)
+			{
+				case 1: cout << "Goalkeeper" << endl; break;
+				case 2: case 3: case 4: cout << "Defender" << endl; break;
+				case 5: case 6: case 7: case 8: cout << "Half-back" << endl; break;
+			}
+	}
+   	system("pause");
+	return 0;
+}
